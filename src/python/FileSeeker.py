f0c10ab0a51d1b172c2cf0ec7889737d75498fb7
Jean Gomes <neutrinomuon@gmail.com>
2023-11-29 17:41:01 +0000

    Set python scripts according to PEP 8 guidelines

--- src/python/FileSeeker.py
+++ src/python/FileSeeker.py
@@ -18,28 +18,34 @@ Written: Jean Michel Gomes © Copyright
 import os
 from termcolor import colored
 
-class FileSeeker( object ):
-
+class FileSeekerClass():
+    '''FileSeekerClass is the class for fileseeker'''
     def __init__(self,search_file,search_dir='./'):
         self.search_dir   = search_dir
         self.search_file  = search_file
 
-        self.seeker_files = self.find_files_with_name(self.search_dir,self.search_file)
+        self.seeker_files = self.find_files_with_name(
+                                                       self.search_dir,
+                                                       self.search_file
+                                                     )
 
         if self.seeker_files:
             print("Files found:")
             for file_path in self.seeker_files:
                 # Highlight search_file in color
-                highlighted_path = file_path.replace(search_file, colored(search_file, 'red'))
+                highlighted_path = file_path.replace(search_file,
+                                   colored(search_file, 'red'))
                 print(highlighted_path)
         else:
-            highlighted_path = search_file.replace(search_file, colored(search_file, 'red'))
-            print(f"No files with the name '{highlighted_path}' found in the directory '{self.search_dir}'.")
-        
-        return
-    
-    # Function to search for files with a given name in a directory and its subdirectories
+            highlighted_path = search_file.replace(search_file,
+                               colored(search_file, 'red'))
+            print(f"No files with the name '{highlighted_path}'"
+                  f"found in the directory '{self.search_dir}'.")
+
+    # Function to search for files with a given name in a \
+    # directory and its subdirectories
     def find_files_with_name(self,directory, search_word):
+        '''Find files with name or part of name'''
         found_files = []
 
         for root, _, files in os.walk(directory):
@@ -50,10 +56,16 @@ class FileSeeker( object ):
 
         return found_files
 
-# Example of how to use
-# Directory to search in
-#search_directory = "./"
-# Search word
-#search_word = "libcfiletype"
-#r_object = FileSeeker( search_word,search_dir=search_directory )
-#r_object.seeker_files
+    def simpletest(self):
+        '''Simple example of how to use'''
+        # Directory to search in local
+        search_directory = "./"
+        # Search a given word or part of word
+        search_word = "libcfiletype"
+        r_object = FileSeekerClass( search_word,
+                                   search_dir=search_directory )
+        print(r_object.seeker_files)
+
+def main():
+    '''Main code'''
+    print("")
